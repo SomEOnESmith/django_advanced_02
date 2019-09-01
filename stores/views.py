f67b5591e0b4dde6c9ea8428f22ffd068e87e03c
Huusain_AlHaddad <h.ali.alhaddad@hotmail.com>
2019-09-01 10:51:08 +0300

    hussain - yousif o

--- stores/views.py
+++ stores/views.py
@@ -11,3 +11,21 @@ def store_list(request):
 
 
 
+def create_store(request):
+    form = StoreModelForm()
+    if request.method == "POST":
+        form = StoreModelForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('list')
+    context = {
+        "form":form,
+    }
+    return render(request, 'create.html', context)
+
+
+def store_detail(request , store_slug):
+	context = {
+		'store' : Store.objects.get(slug= store_slug)
+	}
+	return render(request,'detail.html',context)
